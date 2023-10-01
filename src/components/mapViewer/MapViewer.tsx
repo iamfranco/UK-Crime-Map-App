96f8667ad33c87b98270f0cdb5b2925934b7250a
iamfranco <kinfaifrancochan@gmail.com>
2023-10-01 18:10:19 +0100

    Add circles for street crime locations

--- src/components/mapViewer/MapViewer.tsx
+++ src/components/mapViewer/MapViewer.tsx
@@ -1,9 +1,10 @@
-import { MapContainer, Marker, Polygon, Popup, TileLayer, useMap } from 'react-leaflet'
+import { Circle, MapContainer, Marker, Polygon, Popup, TileLayer, useMap } from 'react-leaflet'
 import { coordinateConversionService } from '../../IoC/serviceProvider';
 import { useContext, useEffect } from 'react';
 import { StreetCrimesContext } from '../../contexts/StreetCrimesProvider';
 import { AddressContext } from '../../contexts/AddressProvider';
 import './MapViewer.css'
+import { StreetCrime } from '../../clients/policeApiClient/models/StreetCrime';
 
 const MapViewer = () => {
   const {address} = useContext(AddressContext);
@@ -33,6 +34,36 @@ const MapAnnotations = () => {
   const pathOptions = { color: '#ff005544' };
   const polygon: [number, number][] = coordinateConversionService
     .getBoundingSquareLatLonPolygon(position, 1000);
+
+  let streetCrimesGroupedByLatLon: StreetCrime[][] = [];
+  for (const s of streetCrimes) {
+    const matchingIndex = streetCrimesGroupedByLatLon.findIndex(x => 
+      x.length > 0 && 
+      x[0].location.latitude == s.location.latitude && 
+      x[0].location.longitude == s.location.longitude);
+
+    if (matchingIndex == -1) {
+      streetCrimesGroupedByLatLon.push([s]);
+      continue;
+    }
+
+    streetCrimesGroupedByLatLon[matchingIndex].push(s);
+  }
+
+  const circles = streetCrimesGroupedByLatLon.map(x => {
+    const circlesGroup = x.map((y) => {
+      const lat = parseFloat(y.location.latitude);
+      const lon = parseFloat(y.location.longitude);
+
+      const position : [number, number] = [lat, lon]
+
+      return (
+        <Circle key={`${y.id}-${y.category}`} center={position} radius={10} />
+      )
+    })
+
+    return circlesGroup;
+  });
   
   return (
     <>
@@ -46,6 +77,7 @@ const MapAnnotations = () => {
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
+      {circles}
     </>
   )
 }
